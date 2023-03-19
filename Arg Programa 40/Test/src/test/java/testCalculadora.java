f181ef3150522057474c622baf84221cf4158b3d
Pablo Damico <zorrito.m44@gmail.com>
2023-03-19 14:00:14 -0300

    claseseis_prueba_beta
    Se sube la version beta de pruebas

---
+++ Arg Programa 40/Test/src/test/java/testCalculadora.java
@@ -0,0 +1,30 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class testCalculadora {
+
+@Test
+public void casoPrueba1() {
+    Calculadora calc = new Calculadora();
+    Assert.assertEquals(10.0,calc.sumar(5,5), 0.0001);
+
+}
+@Test
+public void casoPrueba2() {
+
+    }
+
+@Test
+public void casoPrueba3() {
+
+    }
+@Test
+public void casoPrueba4() {
+
+    }
+
+
+
+}
